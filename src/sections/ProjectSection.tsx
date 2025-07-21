bc094fa4c614d4e344606eb683b5958d0776b63d
onyekweremichael <onyekweremichael55@gmail.com>
2025-07-21 14:47:23 +0100

    Add image type declarations for JPG and PNG formats, update image imports in constants and ProjectSection for consistency

--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-import project1 from '../assets/public/images/project2.png'
+import project1 from '../assets/public/images/interview.JPG'
 import project2 from '/src/assets/public/images/xclux.png'
 import project3 from '../assets/public/images/project3.png'
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module "*.JPG";
+declare module "*.jpg";
+declare module "*.PNG";
+declare module "*.png";
