eb3237b2be10b9896a8d4c225e72258f59a2e7fc
onyekweremichael <onyekweremichael55@gmail.com>
2025-09-14 13:38:46 +0100

    upd

--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -72,7 +72,7 @@ const ProjectSection = () => {
                   business
                 </h2>
                 <p className="text-white-50 md:text-xl leading-8">
-                  An app built with Nextjs, Typescript, and TailwindCSS for a
+                  An app built with Nextjs, Typescript, Expressjs and Nodejs and TailwindCSS for a
                   fast, user-friendly experience.
                 </p>
               </div>
@@ -99,7 +99,7 @@ const ProjectSection = () => {
                   Whitelist, An App that connects property owners with tenants
                 </h2>
                 <p className="text-white-50 md:text-xl leading-8">
-                  An app built with Nextjs, Typescript, and Material UI for a
+                  An app built with Nextjs, Typescript, Expressjs and Nodejs, and Material UI for a
                   fast, user-friendly experience.
                 </p>
               </div>
@@ -188,7 +188,7 @@ const ProjectSection = () => {
                 Nexview – An AI-powered platform to help you ace any interview.
                 </h2>
                 <p className="text-white-50 md:text-xl leading-8">
-                  An app built with Reactjs,  Typescript and
+                  An app built with Reactjs,  Typescript, Vapi, Expressjs and Nodejs
                   TailwindCSS.
                 </p>
               </div>
@@ -215,7 +215,7 @@ const ProjectSection = () => {
                   Xclux, an Ecommerce app that makes shopping easy.
                 </h2>
                 <p className="text-white-50 md:text-xl leading-8">
-                  An app built with Reactjs, Typescript, and TailwindCSS for a
+                  An app built with Reactjs, Typescript,  TailwindCSS, Nodejs, Expressjs, and Mongobd for a
                   fast, user-friendly experience.
                 </p>
               </div>
